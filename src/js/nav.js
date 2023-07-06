2e35986a9b12a0dbf7d752a4b6896d09b9cff0dd
TichFi <juan.valenzuela@tich.fi>
2023-07-06 20:35:03 +0300

    Initial Commit
    The initial commit has a basic custom template that has space to improve.

---
+++ src/js/nav.js
@@ -0,0 +1,82 @@
+
+//  SECTION
+window.addEventListener("scroll", function () {
+    let nav = document.getElementById("nav");
+    nav.classList.toggle("down", window.scrollY > 0);
+});
+window.addEventListener("resize", function () {
+    let linkList = document.querySelector(".navigation");
+    if (window.innerWidth > 1080) {
+        closeMenu();
+    }
+});
+
+//  SECTION
+function toggle() {
+    let header = document.querySelector("header");
+    header.classList.toggle("active");
+}
+
+//Section: ON SCROLL CHANGE NAV LINKS AS ACTIVE
+///VARIABLES
+let section = document.querySelectorAll("section, body > header");
+let navLinks = document.querySelectorAll(".navigation a");
+///EVENT LISTENERS
+window.onscroll = () => {
+    section.forEach((sec) => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 90;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute("id");
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach((links) => {
+                links.classList.remove("active");
+                document
+                    .querySelector(".navigation a[href*=" + id + "]")
+                    .classList.add("active");
+            });
+        } else if (id === "home" && top < + height) {
+            navLinks.forEach((links) => {
+                links.classList.remove("active");
+                document
+                    .querySelector(".navigation a[href*=" + id + "]")
+                    .classList.add("active");
+            });
+        }
+    });
+};
+
+//Section: PHONE SIZE MENU
+///EVENT LISTENERS
+window.addEventListener("resize", function () {
+    let linkList = document.querySelector(".navigation");
+    if (window.innerWidth > 1080) {
+        closeMenu();
+    }
+});
+///FUNCTIONS
+const toggleMenu = () => {
+    let navActive = document.querySelector(".navigation.active");
+    if (navActive) {
+        closeMenu();
+    } else {
+        openMenu();
+    }
+
+}
+const openMenu = () => {
+    let html = document.querySelector("html");
+    let linkList = document.querySelector(".navigation");
+
+    html.classList.add("noOverflow");
+    linkList.classList.add("active");
+    console.log("Test");
+}
+const closeMenu = () => {
+    let html = document.querySelector("html");
+    let linkList = document.querySelector(".navigation");
+
+    html.classList.remove("noOverflow");
+    linkList.classList.remove("active");
+}
