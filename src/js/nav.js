08b4d9593a2f3cd89544a7f6b698cb3ea0bd76fc
TichFi <juan.valenzuela@tich.fi>
2023-07-29 19:51:36 +0300

    Update nav.js

--- src/js/nav.js
+++ src/js/nav.js
@@ -1,56 +1,14 @@
 
-//  SECTION
+//  SECTION: On scroll change main nav height
 window.addEventListener("scroll", function () {
     let nav = document.getElementById("nav");
     nav.classList.toggle("down", window.scrollY > 0);
 });
-window.addEventListener("resize", function () {
-    let linkList = document.querySelector(".navigation");
-    if (window.innerWidth > 1080) {
-        closeMenu();
-    }
-});
 
-//  SECTION
-function toggle() {
-    let header = document.querySelector("header");
-    header.classList.toggle("active");
-}
 
-//Section: ON SCROLL CHANGE NAV LINKS AS ACTIVE
-///VARIABLES
-let section = document.querySelectorAll("section, body > header");
-let navLinks = document.querySelectorAll(".navigation a");
-///EVENT LISTENERS
-window.onscroll = () => {
-    section.forEach((sec) => {
-        let top = window.scrollY;
-        let offset = sec.offsetTop - 90;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute("id");
-
-        if (top >= offset && top < offset + height) {
-            navLinks.forEach((links) => {
-                links.classList.remove("active");
-                document
-                    .querySelector(".navigation a[href*=" + id + "]")
-                    .classList.add("active");
-            });
-        } else if (id === "home" && top < + height) {
-            navLinks.forEach((links) => {
-                links.classList.remove("active");
-                document
-                    .querySelector(".navigation a[href*=" + id + "]")
-                    .classList.add("active");
-            });
-        }
-    });
-};
-
-//Section: PHONE SIZE MENU
+// SECTION: PHONE SIZE MENU
 ///EVENT LISTENERS
 window.addEventListener("resize", function () {
-    let linkList = document.querySelector(".navigation");
     if (window.innerWidth > 1080) {
         closeMenu();
     }
@@ -68,7 +26,7 @@ const toggleMenu = () => {
 const openMenu = () => {
     let html = document.querySelector("html");
     let linkList = document.querySelector(".navigation");
-
+    
     html.classList.add("noOverflow");
     linkList.classList.add("active");
     console.log("Test");
@@ -80,3 +38,36 @@ const closeMenu = () => {
     html.classList.remove("noOverflow");
     linkList.classList.remove("active");
 }
+
+
+
+
+// //Section: ON SCROLL CHANGE NAV LINKS AS ACTIVE
+// ///VARIABLES
+// let section = document.querySelectorAll("section, body > header");
+// let navLinks = document.querySelectorAll(".navigation a");
+// ///EVENT LISTENERS
+// window.onscroll = () => {
+//     section.forEach((sec) => {
+//         let top = window.scrollY;
+//         let offset = sec.offsetTop - 90;
+//         let height = sec.offsetHeight;
+//         let id = sec.getAttribute("id");
+
+//         if (top >= offset && top < offset + height) {
+//             navLinks.forEach((links) => {
+//                 links.classList.remove("active");
+//                 document
+//                     .querySelector(".navigation a[href*=" + id + "]")
+//                     .classList.add("active");
+//             });
+//         } else if (id === "home" && top < + height) {
+//             navLinks.forEach((links) => {
+//                 links.classList.remove("active");
+//                 document
+//                     .querySelector(".navigation a[href*=" + id + "]")
+//                     .classList.add("active");
+//             });
+//         }
+//     });
+// };
